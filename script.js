1b59bb5729435c8cbfcbe8f593e48837e48c1889
Flamingod5 <140133760+Flamigod5@users.noreply.github.com>
2025-11-11 13:24:41 +1100

    Update script.js
    added image links

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
 const entries = [
   { text: "Chopstick Vietnames Cuisine", image: "chopstick-vietnames-cuisine" },
-  { text: "GK Yeero's", image: "gk-yeeros" },
-  { text: "Guzman Y Gomez", image: "guzman-y-gomez" },
-  { text: "Jim's Malaysian", image: "jims-malaysian" },
-  { text: "Johnny Gio's", image: "johnny-gios" },
-  { text: "Kingsway Curry", image: "kingsway-curry" },
-  { text: "Kuroneko", image: "kuroneko" },
-  { text: "Petra Lebanese Cuisine", image: "petra-lebanese-cuisine" },
-  { text: "Somewhere Completely New", image: "somewhere-completely-new" },
-  { text: "Vitaly Italian Restraunt", image: "vitaly-italian-restraunt" }
+  { text: "GK Yeero's", image: "gk-yeeros.jpg" },
+  { text: "Guzman Y Gomez", image: "guzman-y-gomez.png" },
+  { text: "Jim's Malaysian", image: "jims-malaysian.jpg" },
+  { text: "Johnny Gio's", image: "johnny-gios.jpeg" },
+  { text: "Kingsway Curry", image: "kingsway-curry.jpg" },
+  { text: "Kuroneko", image: "kuroneko.jpg" },
+  { text: "Petra Lebanese Cuisine", image: "petra-lebanese-cuisine.jpeg" },
+  { text: "Vitaly Italian Restraunt", image: "vitaly-italian-restraunt.jpg" },
+  
+  { text: "Somewhere Completely New", image: "somewhere-completely-new.gif" }
 ];
 
   const button = document.getElementById("revealButton");
